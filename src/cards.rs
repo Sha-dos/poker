609bb9ca2711643ccc0e5006ead0dd9074a1f772
Nolan <77746688+Sha-dos@users.noreply.github.com>
2022-10-13 16:43:02 -0500

    push

--- src/cards.rs
+++ src/cards.rs
@@ -1,18 +1,87 @@
-#[derive(Clone, Copy, Debug)]
+use crate::poker_error::Error;
+
+#[derive(Clone, Debug)]
 pub struct Hand {
-    Vec<Card>
+    cards: Vec<Card>,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 pub struct Card {
-    number: i32
-    
+    card_type: Type,
+    suit: Suit,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 pub enum Suit {
     Club,
     Diamond,
     Heart,
     Spade,
+}
+
+#[derive(Clone, Debug)]
+pub enum Type {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Null,
+}
+
+impl Hand {
+    fn default() -> Hand {
+        let cards = vec![Card { card_type: Type::Ace, suit: Suit::Club }];
+        Hand {
+            cards
+        }
+    }
+}
+
+impl Card {
+    fn to_card(card_type: char, suit: Suit) -> Result<Card, Error> {
+        let mut _card_type = Type::Null;
+
+        match card_type {
+            'A' => _card_type = Type::Ace,
+            '2' => _card_type = Type::Ace,
+            '3' => _card_type = Type::Ace,
+            '4' => _card_type = Type::Ace,
+            '5' => _card_type = Type::Ace,
+            '6' => _card_type = Type::Ace,
+            '7' => _card_type = Type::Ace,
+            '8' => _card_type = Type::Ace,
+            '9' => _card_type = Type::Ace,
+            'T' => _card_type = Type::Ace,
+            'J' => _card_type = Type::Ace,
+            'Q' => _card_type = Type::Ace,
+            'K' => _card_type = Type::Ace,
+            _ => return Error::FailedToParse,
+        }
+
+        Ok(Card { card_type: (), suit: () })
+    }
+}
+
+pub fn parse_cards(line: String) -> Result<(), Error> {
+    let vector: Vec<&str> = line.split_whitespace().collect();
+
+    let parsed: Vec<Card> =
+
+    let mut p1: Hand = Hand::default();
+    let mut p2: Hand = Hand::default();
+
+    for i in vector {
+        println!("{}", i);
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod cards;
+mod poker_error;
+
 fn main() {
-    println!("Hello, world!");
+    cards::parse_cards("8C TS KC 9H 4S 7D 2S 5D 3S AC".to_string());
 }
